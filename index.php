8986bfefd58698d999b7e71630a657e6465fee6e
diakhala <diakhalakonte97@gmail.com>
2025-07-21 11:38:49 +0000

    push de projet

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $users = $pdo->query("SELECT * FROM users");
 </head>
 <body>
     <h1>Liste des utilisateurs</h1>
-    <a href="create.php">➕ Ajouter un utilisateurs</a>
+    <a href="create.php">➕ Ajouter des utilisateur</a>
     <table>
         <tr><th>ID</th><th>Nom</th><th>Email</th><th>Actions</th></tr>
         <?php foreach ($users as $user): ?>
